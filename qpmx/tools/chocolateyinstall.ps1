7db9887ac0033164cb0d683415b302556c78575f
Skycoder42 <Skycoder42@users.noreply.github.com>
2018-09-19 16:10:06 +0200

    added qpmx

---
+++ qpmx/tools/chocolateyinstall.ps1
@@ -0,0 +1,13 @@
+﻿
+$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  url64bit      = "https://github.com/Skycoder42/qpmx/releases/download/${env:ChocolateyPackageVersion}/qpmx_win_deployed.zip"
+  checksum64    = '5d54939bdd084591d6a3f75ea169e1d5ee3b943a9c7e54508372fac3e1d0d5f0'
+  checksumType64= 'sha256'
+}
+
+Install-ChocolateyZipPackage @packageArgs
